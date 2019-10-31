7623da798a3d49352f964b6561b134af4fceebe7
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2019-10-30 20:16:27 -0400

    5 Passed

--- index.js
+++ index.js
@@ -75,8 +75,16 @@ function getName(person) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-	/* code here */
+function makeSmartPerson(name) {
+	return {
+		name: name,
+		sum: function(a, b) {
+			return a + b;
+		},
+		speaks: function() {
+			return `Hello my name is ${name}`;
+		}
+	};
 }
 
 /**
